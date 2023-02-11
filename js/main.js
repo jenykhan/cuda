1ef465512c20ca1772d2d782d70413ba4474d80a
Afroza khanam jeny <afrozakhanomjeny@gmail.com>
2023-02-11 09:32:45 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+   //Easy pie chart activator
+   $(function() {
+    $('.chart').easyPieChart({
+       lineWidth: 15,
+       size: 162,
+       lineCap:"square"
+    });
+});
+});
